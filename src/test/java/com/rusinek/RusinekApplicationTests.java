14d7b0622fc3c809f0222175b222ddada0cb18d7
AdRusinek <rusinekadrian19@gmail.com>
2020-03-16 01:55:42 +0100

    Initial commit

---
+++ src/test/java/com/rusinek/RusinekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rusinek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RusinekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
